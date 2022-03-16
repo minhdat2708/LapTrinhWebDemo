e73f7d3e661d87c4dcd78b2b7b56a28cd65b17ce
minhdat2708 <minhdat.maximus@gmail.com>
2022-03-17 02:10:14 +0700

    update project

---
+++ Controllers/SanPhamController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+using DemoWebBanVali.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace DemoWebBanVali.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        // GET: SanPham
+        WebBanVaLiEntities db = new WebBanVaLiEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult LoaiPartial()
+        {
+            return PartialView(db.tLoaiSPs.ToList());
+        }
+
+        public ViewResult SanPhamTheoLoai(int? page, string MaLoai = "vali")
+        {
+            int pageSize = 12; //Số sản phẩm trong một trang
+            int pagenum = (page ?? 1);
+
+            tLoaiSP loaiSanPham = db.tLoaiSPs.SingleOrDefault(n => n.MaLoai == MaLoai); 
+            if (loaiSanPham == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            // Khai báo một sản phẩm trong bảng danh mục sản phẩm
+            List<tDanhMucSP> lstSanPham = db.tDanhMucSPs.Where(n => n.MaLoai == MaLoai).OrderBy(n=>n.TenSP).ToList();
+            if (lstSanPham.Count == 0)
+            {
+                ViewBag.lstSanPham = "Không có sản phẩm thuộc loại này!";
+            }
+            ViewBag.lstSanPham = db.tDanhMucSPs.ToList();
+            ViewBag.MaLoai = MaLoai;
+            return View(lstSanPham.ToPagedList(pagenum, pageSize));
+        }
+
+        public ViewResult ChiTietSanPham(string MaSP = "bacackeroirbl")
+        {
+            tDanhMucSP sanPham = db.tDanhMucSPs.SingleOrDefault(n=>n.MaSP == MaSP);
+            if (sanPham == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sanPham);
+        }
+
+        [HttpGet]
+        public ActionResult ThemSanPham()
+        {
+            ViewBag.MaChatLieu = new SelectList(db.tChatLieux.ToList().OrderBy(n=>n.ChatLieu), "MaChatLieu", "ChatLieu");
+            ViewBag.MaKichThuoc = new SelectList(db.tKichThuocs.ToList().OrderBy(n=>n.KichThuoc), "MaKichThuoc", "KichThuoc");
+            ViewBag.MaHangSX = new SelectList(db.tHangSXes.ToList().OrderBy(n=>n.HangSX), "MaHangSX", "HangSX");
+            ViewBag.MaNuocSX = new SelectList(db.tQuocGias.ToList().OrderBy(n=>n.TenNuoc), "MaNuoc", "TenNuoc");
+            ViewBag.MaLoai = new SelectList(db.tLoaiSPs.ToList().OrderBy(n=>n.Loai), "MaLoai", "Loai");
+            ViewBag.MaDT = new SelectList(db.tLoaiDTs.ToList().OrderBy(n=>n.TenLoai), "MaDT", "TenLoai");
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ThemSanPham([Bind(Include = "MaSP, TenSP, MaChatLieu, NganLapTop, Model, MauSac, MaKichThuoc, CanNang, DoNoi, MaHangSX, MaNuocSX, MaDacTinh, Website, ThoiGianBaoHanh,GioiThieuSP, Gia, ChietKhau, MaLoai, MaDT, Anh")] tDanhMucSP sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tDanhMucSPs.Add(sanPham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sanPham);
+        }
+
+        [HttpGet]
+        public ActionResult SuaSanPham(String MaSP)
+        {
+            if (MaSP == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            tDanhMucSP sanPham = db.tDanhMucSPs.Find(MaSP);
+            if (sanPham == null)
+            {
+                return HttpNotFound(); 
+            }
+            ViewBag.MaChatLieu = new SelectList(db.tChatLieux.ToList().OrderBy(n => n.ChatLieu), "MaChatLieu", "ChatLieu");
+            ViewBag.MaKichThuoc = new SelectList(db.tKichThuocs.ToList().OrderBy(n => n.KichThuoc), "MaKichThuoc", "KichThuoc");
+            ViewBag.MaHangSX = new SelectList(db.tHangSXes.ToList().OrderBy(n => n.HangSX), "MaHangSX", "HangSX");
+            ViewBag.MaNuocSX = new SelectList(db.tQuocGias.ToList().OrderBy(n => n.TenNuoc), "MaNuoc", "TenNuoc");
+            ViewBag.MaLoai = new SelectList(db.tLoaiSPs.ToList().OrderBy(n => n.Loai), "MaLoai", "Loai");
+            ViewBag.MaDT = new SelectList(db.tLoaiDTs.ToList().OrderBy(n => n.TenLoai), "MaDT", "TenLoai");
+            return View(sanPham);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SuaSanPham([Bind(Include = "MaSP, TenSP, MaChatLieu, NganLapTop, Model, MauSac, MaKichThuoc, CanNang, DoNoi, MaHangSX, MaNuocSX, MaDacTinh, Website, ThoiGianBaoHanh,GioiThieuSP, Gia, ChietKhau, MaLoai, MaDT, Anh")] tDanhMucSP sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sanPham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult XoaSanPham(String MaSP)
+        {
+            tDanhMucSP sanPham = db.tDanhMucSPs.Single(n => n.MaSP == MaSP);
+            if (sanPham == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sanPham);
+        }
+
+        [HttpPost, ActionName("XoaSanPham")]
+        public ActionResult XacNhanXoa (string MaSP)
+        {
+            tDanhMucSP sanPham = db.tDanhMucSPs.Single(n => n.MaSP == MaSP);
+            var anhSP = from p in db.tAnhSPs where p.MaSP == sanPham.MaSP select p;
+            if (sanPham == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.tAnhSPs.RemoveRange(anhSP);
+            db.tDanhMucSPs.Remove(sanPham);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Metadatas/tDanhMucSP.Metadata.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoWebBanVali.Models
+{
+    [MetadataTypeAttribute(typeof(tDanhMucSPMetadata))]
+    public partial class tDanhMucSP
+    {
+        internal sealed class tDanhMucSPMetadata
+        {
+            [Display(Name = "Mã sản phẩm")]
+            [Required(ErrorMessage = "Vui lòng nhập giá trị cho trường này!")]
+            public string MaSP { get; set; }
+            [Display(Name = "Tên sản phẩm")]
+            public string TenSP { get; set; }
+            [Display(Name = "Ngăn laptop")]
+            public string NganLapTop { get; set; }
+            [Display(Name = "Model")]
+            public string Model { get; set; }
+            [Display(Name = "Màu sắc")]
+            public string MauSac { get; set; }
+            [Display(Name = "Mã kích thước")]
+            public string MaKichThuoc { get; set; }
+            [Display(Name = "Cân nặng")]
+            public Nullable<double> CanNang { get; set; }
+            [Display(Name = "Độ nới")]
+            public Nullable<double> DoNoi { get; set; }
+            [Display(Name = "Thời gian bảo hành")]
+            public Nullable<double> ThoiGianBaoHanh { get; set; }
+            [Display(Name = "Giới thiệu")]
+            public string GioiThieuSP { get; set; }
+            [Display(Name = "Giá")]
+            public Nullable<double> Gia { get; set; }
+            [Display(Name = "Chiết khấu")]
+            public Nullable<double> ChietKhau { get; set; }
+        }
+
+    }
+}
